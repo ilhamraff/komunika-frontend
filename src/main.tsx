daa96b0a6d3e1a73300b79d28c46d45c36b6e57b
ilhamraff <ifadhilah261@gmail.com>
2025-08-27 01:31:52 +0700

    feat: add routing and state management with React Query and Axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <p>Hello World</p>
-    {/* <App /> */}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
