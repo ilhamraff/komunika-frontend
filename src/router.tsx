706a54d69466df06a2af4850626bde31191479f4
ilhamraff <ifadhilah261@gmail.com>
2025-08-27 02:06:33 +0700

    feat: enhance styling and animations in index.css; update router to use LandingPage component

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router";
+import LandingPage from "./features/landing/pages/LandingPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>Landing Page</p>,
+    element: <LandingPage />,
   },
 ]);
 
